dbcd36e64feba5e2e68b5a64b3cf09d53dfe4d07
cggtabares <carlosggtabares@gmail.com>
2023-08-07 00:28:44 -0400

    Hola Mundo

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,26 @@
+#Esto es un comentario:
+#Nuestro Hola Mundo
 print("Hello World!")
+print('Hello World!')
+
+print(type("Hola Python"))
+
+"""
+Comentario
+en 
+varias
+lineas
+"""
+'''
+Este
+tambien es un 
+comentario
+en varias lineas
+'''
+
+#Consultar el tipo de dato
+print(type("Hola Python")) #tipo str
+print(type(5)) #tipo int
+print(type(1.4)) #tipo float
+print(type(True)) #tipo bool
+print(type(1+2j)) #tipo complejo
